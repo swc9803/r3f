ee5fe23062f04751cb996a325d21321447498f29
swc9803 <swc9803@gmail.com>
2024-01-09 04:22:42 +0900

    children mesh test

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,8 +1,14 @@
 import { useRef } from 'react';
 import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber';
 
 const MyElement3D = () => {
   const refMesh = useRef();
+
+  useFrame((state, delta) => {
+    refMesh.current.rotation.z += delta;
+  });
 
   return (
     <>
@@ -11,9 +17,18 @@ const MyElement3D = () => {
       <axesHelper scale={10} />
       <OrbitControls />
 
-      <mesh ref={refMesh}>
+      <mesh
+        ref={refMesh}
+        position={[2, 0, 0]}
+        rotation-y={THREE.MathUtils.degToRad(45)}
+        scale={[2, 1, 1]}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="#e67e22" />
+        <meshStandardMaterial color="#e67e22" opacity={0.5} transparent={true} />
+        <mesh scale={[0.1, 0.1, 0.1]}>
+          <sphereGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
       </mesh>
     </>
   );
