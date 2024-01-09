e6658285ade6357ab37d94c4f209bd2a3ad424e3
swc9803 <swc9803@gmail.com>
2024-01-10 02:21:18 +0900

    ui로 변경한 값을  useEffect로 적용

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,4 +1,5 @@
 import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
 import { useEffect, useRef } from 'react';
 // import * as THREE from 'three';
 
@@ -6,9 +7,18 @@ const MyElement3D = () => {
   const refMesh = useRef();
   const refWireMesh = useRef();
 
+  const { xSize, ySize, zSize, xSegments, ySegments, zSegments } = useControls({
+    xSize: { value: 1, min: 0.1, max: 5, step: 0.01 },
+    ySize: { value: 1, min: 0.1, max: 5, step: 0.01 },
+    zSize: { value: 1, min: 0.1, max: 5, step: 0.01 },
+    xSegments: { value: 1, min: 1, max: 10, step: 1 },
+    ySegments: { value: 1, min: 1, max: 10, step: 1 },
+    zSegments: { value: 1, min: 1, max: 10, step: 1 },
+  });
+
   useEffect(() => {
     refWireMesh.current.geometry = refMesh.current.geometry;
-  }, []);
+  }, [xSize, ySize, zSize, xSegments, ySegments, zSegments]);
 
   return (
     <>
@@ -18,7 +28,7 @@ const MyElement3D = () => {
       <directionalLight position={[2, 1, 3]} intensity={0.5} />
 
       <mesh ref={refMesh}>
-        <boxGeometry args={[2, 2, 1, 3, 1, 3]} />
+        <boxGeometry args={[xSize, ySize, zSize, xSegments, ySegments, zSegments]} />
         <meshStandardMaterial color="#1abc9c" />
       </mesh>
 
