6a16249da204299e3709f58ebc67a7420b0af6e0
swc9803 <swc9803@gmail.com>
2024-01-10 02:16:20 +0900

    useRef geometry 변경

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,33 +1,30 @@
-import { Box, OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
-
-const MyBox = (props) => {
-  const geom = new THREE.BoxGeometry();
-  return <mesh {...props} geometry={geom}></mesh>;
-};
+import { OrbitControls } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+// import * as THREE from 'three';
 
 const MyElement3D = () => {
+  const refMesh = useRef();
+  const refWireMesh = useRef();
+
+  useEffect(() => {
+    refWireMesh.current.geometry = refMesh.current.geometry;
+  }, []);
+
   return (
     <>
       <OrbitControls />
+
       <ambientLight intensity={0.1} />
       <directionalLight position={[2, 1, 3]} intensity={0.5} />
 
-      {/* 1 */}
-      <mesh>
-        <boxGeometry />
+      <mesh ref={refMesh}>
+        <boxGeometry args={[2, 2, 1, 3, 1, 3]} />
         <meshStandardMaterial color="#1abc9c" />
       </mesh>
 
-      {/* 2 three.js 사용 */}
-      <Box position={[1.2, 0, 0]}>
-        <meshStandardMaterial color="#8e44ad" />
-      </Box>
-
-      {/* 3 컴포넌트 사용 */}
-      <MyBox position={[-1.2, 0, 0]}>
-        <meshStandardMaterial color="#e74c3c" />
-      </MyBox>
+      <mesh ref={refWireMesh}>
+        <meshStandardMaterial emissive="yellow" wireframe={true} />
+      </mesh>
     </>
   );
 };
