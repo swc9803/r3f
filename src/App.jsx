d32ec2f986cd1716e939eb19c27164af4503762e
swc9803 <swc9803@gmail.com>
2024-01-12 00:01:19 +0900

    array group

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import MyElement3D from './MyElement3D';
 const App = () => {
   return (
     <div>
-      <Canvas>
+      <Canvas
+        camera={{
+          fov: 75,
+          position: [7, 7, 0],
+        }}
+      >
         <MyElement3D />
       </Canvas>
     </div>
--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,34 +1,59 @@
-import { MeshReflectorMaterial, OrbitControls } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+
+const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32);
+const torusMaterial = new THREE.MeshStandardMaterial({
+  color: '#9b59b6',
+  roughness: 0.5,
+  metalness: 0.9,
+});
 
 const MyElement3D = () => {
+  useFrame((state) => {
+    const time = state.clock.elapsedTime;
+    const smallSpherePivot = state.scene.getObjectByName('smallSpherePivot');
+    smallSpherePivot.rotation.y = THREE.MathUtils.degToRad(time * 50);
+  });
+
   return (
     <>
       <OrbitControls />
 
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0, 1, 0]} />
-      <directionalLight position={[1, 2, 8]} intensity={0.7} />
+      <ambientLight color="#ffffff" intensity={5} />
 
-      <mesh position={[0, -0.6, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <mesh rotation-x={THREE.MathUtils.degToRad(-90)}>
         <planeGeometry args={[10, 10]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={30}
-          roughness={1}
-          depthScale={0.7}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color="#050505"
+        <meshStandardMaterial
+          color="#2c3e50"
+          roughness={0.5}
           metalness={0.5}
+          side={THREE.DoubleSide}
         />
       </mesh>
 
-      <mesh position={[0, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="cyan" />
+      <mesh rotation-x={THREE.MathUtils.degToRad(-90)}>
+        <sphereGeometry args={[1.5, 64, 64, 0, Math.PI]} />
+        <meshStandardMaterial color="#fffffff" roughness={0.1} metalness={0.2} />
       </mesh>
+      {new Array(8).fill().map((item, index) => {
+        return (
+          <group key={index} rotation-y={THREE.MathUtils.degToRad(45 * index)}>
+            <mesh
+              geometry={torusGeometry}
+              material={torusMaterial}
+              position={[3, 0.5, 0]}
+            />
+          </group>
+        );
+      })}
+
+      <group name="smallSpherePivot">
+        <mesh position={[3, 0.5, 0]}>
+          <sphereGeometry args={[0.3, 32, 32]} />
+          <meshStandardMaterial color="#e74c3c" roughness={0.2} metalness={0.5} />
+        </mesh>
+      </group>
     </>
   );
 };
