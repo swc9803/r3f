a8c5858d7a9d817d859b38bccff8804f974ebd5e
swc9803 <swc9803@gmail.com>
2024-01-13 20:14:32 +0900

    AccumulativeShadows

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,6 +1,5 @@
-import { OrbitControls } from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber';
-import { useEffect, useRef } from 'react';
+import { AccumulativeShadows, OrbitControls, RandomizedLight } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32);
@@ -15,20 +14,7 @@ const MyElement3D = () => {
     const time = state.clock.elapsedTime;
     const smallSpherePivot = state.scene.getObjectByName('smallSpherePivot');
     smallSpherePivot.rotation.y = THREE.MathUtils.degToRad(time * 10);
-
-    smallSpherePivot.children[0].getWorldPosition(light.current.target.position);
   });
-
-  const light = useRef();
-
-  const { scene } = useThree();
-
-  useEffect(() => {
-    scene.add(light.current.target);
-    return () => {
-      scene.remove(light.current.target);
-    };
-  }, [light.current]);
 
   return (
     <>
@@ -36,27 +22,9 @@ const MyElement3D = () => {
 
       <ambientLight intensity={0.1} />
 
-      <spotLight
-        ref={light}
-        shadow-mapSize={[1024 * 4, 1024 * 4]}
-        castShadow
-        color={0xffffff}
-        intensity={0.9}
-        position={[0, 5, 0]}
-        angle={THREE.MathUtils.degToRad(60)}
-      />
+      <directionalLight color="#ffffff" intensity={1} position={[0, 5, 0]} />
 
-      <mesh receiveShadow rotation-x={THREE.MathUtils.degToRad(-90)}>
-        <planeGeometry args={[10, 10]} />
-        <meshStandardMaterial
-          color="#2c3e50"
-          roughness={0.5}
-          metalness={0.5}
-          side={THREE.DoubleSide}
-        />
-      </mesh>
-
-      <mesh castShadow receiveShadow position-y={1.7}>
+      <mesh castShadow position-y={1.7}>
         <torusKnotGeometry args={[1, 0.2, 128, 32]} />
         <meshStandardMaterial color="#ffffff" roughness={0.1} metalness={0.2} />
       </mesh>
@@ -66,7 +34,6 @@ const MyElement3D = () => {
           <group key={index} rotation-y={THREE.MathUtils.degToRad(45 * index)}>
             <mesh
               castShadow
-              receiveShadow
               geometry={torusGeometry}
               material={torusMaterial}
               position={[3, 0.5, 0]}
@@ -75,14 +42,37 @@ const MyElement3D = () => {
         );
       })}
 
-      <axesHelper />
-
       <group name="smallSpherePivot">
-        <mesh position={[3, 0.5, 0]}>
+        <mesh castShadow position={[3, 0.5, 0]}>
           <sphereGeometry args={[0.3, 32, 32]} />
           <meshStandardMaterial color="#e74c3c" roughness={0.2} metalness={0.5} />
         </mesh>
       </group>
+
+      <AccumulativeShadows
+        position={[0, 0.01, 0]}
+        scale={12}
+        color="#000000"
+        opacity={0.7}
+        alphaTest={1}
+        frames={Infinity}
+        temporal
+        blend={30}
+      >
+        <RandomizedLight
+          radius={0.5}
+          ambient={0.21}
+          intensity={1.5}
+          position={[5, 3, 0]}
+        />
+        {/* <RandomizedLight
+          amount={4}
+          radius={0.5}
+          ambient={0.21}
+          intensity={0.6}
+          position={[-5, 3, 5]}
+        /> */}
+      </AccumulativeShadows>
     </>
   );
 };
