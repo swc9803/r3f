e870cd89e1815340c548b43e8f4e98798b0b195e
swc9803 <swc9803@gmail.com>
2024-01-13 20:22:26 +0900

    SoftShadows

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,5 +1,6 @@
-import { AccumulativeShadows, OrbitControls, RandomizedLight } from '@react-three/drei';
+import { OrbitControls, SoftShadows } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import { useControls } from 'leva';
 import * as THREE from 'three';
 
 const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32);
@@ -16,15 +17,33 @@ const MyElement3D = () => {
     smallSpherePivot.rotation.y = THREE.MathUtils.degToRad(time * 10);
   });
 
+  const config = useControls({
+    size: { value: 25, min: 0, max: 100 },
+    focus: { value: 0, min: 0, max: 10 },
+    samples: { value: 10, min: 1, max: 100, step: 1 },
+  });
+
   return (
     <>
       <OrbitControls />
 
+      <SoftShadows {...config} />
+
       <ambientLight intensity={0.1} />
 
-      <directionalLight color="#ffffff" intensity={1} position={[0, 5, 0]} />
+      <directionalLight castShadow color="#ffffff" intensity={1} position={[0, 5, 0]} />
 
-      <mesh castShadow position-y={1.7}>
+      <mesh receiveShadow rotation-x={THREE.MathUtils.degToRad(-90)}>
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial
+          color="#2c3e50"
+          roughness={0.5}
+          metalness={0.5}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+
+      <mesh castShadow receiveShadow position-y={1.7}>
         <torusKnotGeometry args={[1, 0.2, 128, 32]} />
         <meshStandardMaterial color="#ffffff" roughness={0.1} metalness={0.2} />
       </mesh>
@@ -34,6 +53,7 @@ const MyElement3D = () => {
           <group key={index} rotation-y={THREE.MathUtils.degToRad(45 * index)}>
             <mesh
               castShadow
+              receiveShadow
               geometry={torusGeometry}
               material={torusMaterial}
               position={[3, 0.5, 0]}
@@ -43,36 +63,11 @@ const MyElement3D = () => {
       })}
 
       <group name="smallSpherePivot">
-        <mesh castShadow position={[3, 0.5, 0]}>
+        <mesh castShadow receiveShadow position={[3, 0.5, 0]}>
           <sphereGeometry args={[0.3, 32, 32]} />
           <meshStandardMaterial color="#e74c3c" roughness={0.2} metalness={0.5} />
         </mesh>
       </group>
-
-      <AccumulativeShadows
-        position={[0, 0.01, 0]}
-        scale={12}
-        color="#000000"
-        opacity={0.7}
-        alphaTest={1}
-        frames={Infinity}
-        temporal
-        blend={30}
-      >
-        <RandomizedLight
-          radius={0.5}
-          ambient={0.21}
-          intensity={1.5}
-          position={[5, 3, 0]}
-        />
-        {/* <RandomizedLight
-          amount={4}
-          radius={0.5}
-          ambient={0.21}
-          intensity={0.6}
-          position={[-5, 3, 5]}
-        /> */}
-      </AccumulativeShadows>
     </>
   );
 };
