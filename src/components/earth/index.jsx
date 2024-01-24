5d5a53a1cd798d70d703746ab31d884d625418c2
swc9803 <swc9803@gmail.com>
2024-01-24 17:14:19 +0900

    star, rotation 추가

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,5 +1,5 @@
-import { useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { useFrame, useLoader } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
 import { TextureLoader } from 'three';
 import * as THREE from 'three';
 
@@ -7,6 +7,7 @@ import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
 import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
 import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg';
 import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg';
+import { useRef } from 'react';
 
 const Earth = () => {
   const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [
@@ -16,12 +17,25 @@ const Earth = () => {
     EarthCloudsMap,
   ]);
 
+  const earthRef = useRef();
+  const cloudRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    earthRef.current.rotation.y = elapsedTime / 10;
+    cloudRef.current.rotation.y = elapsedTime / 8;
+  });
+
   return (
     <>
-      <ambientLight intensity={2} />
+      {/* <ambientLight intensity={2} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={12.2} />
+
+      <Stars radius={300} depth={60} count={5000} factor={7} saturation={0} fade={true} />
 
       {/* 구름 */}
-      <mesh>
+      <mesh ref={cloudRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -33,10 +47,15 @@ const Earth = () => {
       </mesh>
 
       {/* 지구 */}
-      <mesh>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
       </mesh>
 
       <OrbitControls
