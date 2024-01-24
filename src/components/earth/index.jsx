3706b48d231eec1a566d291a572fa07ca3277410
swc9803 <swc9803@gmail.com>
2024-01-24 16:22:25 +0900

    orbitControls, cloud texture 추가

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,9 +1,12 @@
 import { useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { TextureLoader } from 'three';
+import * as THREE from 'three';
+
 import EarthDayMap from '../../assets/textures/8k_earth_daymap.jpg';
 import EarthNormalMap from '../../assets/textures/8k_earth_normal_map.jpg';
 import EarthSpecularMap from '../../assets/textures/8k_earth_specular_map.jpg';
 import EarthCloudsMap from '../../assets/textures/8k_earth_clouds.jpg';
-import { TextureLoader } from 'three';
 
 const Earth = () => {
   const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [
@@ -15,12 +18,35 @@ const Earth = () => {
 
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={2} />
+
+      {/* 구름 */}
+      <mesh>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+
+      {/* 지구 */}
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial map={colorMap} normalMap={normalMap} />
       </mesh>
+
+      <OrbitControls
+        enableZoom={true}
+        enablePan={true}
+        enableRotate={true}
+        zoomSpeed={0.6}
+        panSpeed={0.5}
+        rotateSpeed={0.4}
+      />
     </>
   );
 };
