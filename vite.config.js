8d024d6afc19689a65ae6c273c851f74b47f6939
swc9803 <swc9803@gmail.com>
2024-01-27 02:32:51 +0900

    install glsl plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glsl()],
 });
