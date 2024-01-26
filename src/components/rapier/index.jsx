e59d14077fe117abcc53bd488c30bbab8034a289
swc9803 <swc9803@gmail.com>
2024-01-26 23:37:04 +0900

    KeyboardControls 추가

--- src/components/rapier/index.jsx
+++ src/components/rapier/index.jsx
@@ -1,21 +1,41 @@
 import { Canvas } from '@react-three/fiber';
-import './index.css';
 import { Physics } from '@react-three/rapier';
 import Experience from './Experience';
-import { Suspense } from 'react';
+import { Suspense, useMemo } from 'react';
+import { KeyboardControls } from '@react-three/drei';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const Controls = {
+  forward: 'forward',
+  back: 'back',
+  left: 'left',
+  right: 'right',
+  jump: 'jump',
+};
 
 const App = () => {
+  const map = useMemo(
+    () => [
+      { name: Controls.forward, keys: ['ArrowUp', 'KeyW'] },
+      { name: Controls.back, keys: ['ArrowDown', 'KeyS'] },
+      { name: Controls.left, keys: ['ArrowLeft', 'KeyA'] },
+      { name: Controls.right, keys: ['ArrowRight', 'KeyD'] },
+      { name: Controls.jump, keys: ['Space'] },
+    ],
+    [],
+  );
+
   return (
-    <div>
+    <KeyboardControls map={map}>
       <Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
         <color attach="background" args={['#ececec']} />
         <Suspense>
-          <Physics debug>
+          <Physics debug gravity={[0, -4, 0]}>
             <Experience />
           </Physics>
         </Suspense>
       </Canvas>
-    </div>
+    </KeyboardControls>
   );
 };
 
