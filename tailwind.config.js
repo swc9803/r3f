abfdf795df5577edced56750e403062387af250e
swc9803 <swc9803@gmail.com>
2024-01-29 10:54:42 +0900

    setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      serif: ['Playfair Display', ...defaultTheme.fontFamily.serif],
-      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-    },
+    extend: {},
   },
   plugins: [],
 };
