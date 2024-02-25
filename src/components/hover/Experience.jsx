27f40b0323db6a77416907fd3433b3d5b86995da
swc9803 <swc9803@gmail.com>
2024-02-26 03:42:39 +0900

    OrbitControls 제거

--- src/components/hover/Experience.jsx
+++ src/components/hover/Experience.jsx
@@ -1,11 +1,9 @@
-import { OrbitControls } from '@react-three/drei';
 import { FadingImage } from './FadingImage';
 import { FadingImageDisplacement } from './FadingImageDisplacement';
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
       <FadingImageDisplacement position-x={1.5} />
       <FadingImage position-x={-1.5} />
     </>
