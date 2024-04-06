cb7cdb9b18e242663b2431430419967c1cb20995
swc9803 <swc9803@gmail.com>
2024-04-06 15:56:32 +0900

    ui 추가

--- src/components/camping/UI.jsx
+++ src/components/camping/UI.jsx
@@ -1,21 +1,15 @@
 import { atom, useAtom } from 'jotai';
+import './style.scss';
 
 export const currentPageAtom = atom('intro');
 
 export const UI = () => {
   const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
   return (
-    <div className="fixed inset-0 pointer-events-none">
-      <section
-        className={`flex w-full h-full flex-col items-center justify-center 
-      duration-500
-      ${currentPage === 'home' ? '' : 'opacity-0'}`}
-      >
+    <div className="ui-container">
+      <section className={`ui-section ${currentPage === 'home' ? '' : 'hidden'}`}>
         <div className="h-[66%]"></div>
-        <button
-          onClick={() => setCurrentPage('store')}
-          className="pointer-events-auto py-4 px-8 bg-orange-400 text-white font-black rounded-full hover:bg-orange-600 cursor-pointer transition-colors duration-500"
-        >
+        <button onClick={() => setCurrentPage('store')} className="button-enter">
           ENTER
         </button>
       </section>
