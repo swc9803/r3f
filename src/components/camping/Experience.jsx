9226e93177bb6cab3aca7ee7dcc5b3f1e2c573e2
swc9803 <swc9803@gmail.com>
2024-04-06 15:11:14 +0900

    add three-fiber text

--- src/components/camping/Experience.jsx
+++ src/components/camping/Experience.jsx
@@ -1,13 +1,39 @@
-import { OrbitControls, Text } from '@react-three/drei';
+import { CameraControls, Text, Environment } from '@react-three/drei';
+import { Camping } from './Camping';
+import { degToRad } from 'three/src/math/MathUtils';
+import { useEffect, useRef } from 'react';
 
 export const Experience = () => {
+  const controls = useRef();
+
+  const intro = async () => {
+    controls.current.dolly(-22);
+    controls.current.smoothTime = 1.6;
+    controls.current.dolly(22, true);
+  };
+
+  useEffect(() => {
+    intro();
+  }, []);
+
   return (
     <>
-      <OrbitControls />
-      <Text font={'fonts/Poppins-Black.ttf'}>
+      <CameraControls ref={controls} />
+      <Text
+        font={'fonts/Poppins-Black.ttf'}
+        position={[-1.3, -0.5, 1]}
+        lineHeight={0.8}
+        textAlign="center"
+        rotation-y={degToRad(30)}
+        anchorY={'bottom'}
+      >
         MY LITTLE{'\n'}CAMPING
         <meshBasicMaterial color={'white'} />
       </Text>
+      <group rotation-y={-degToRad(25)} position-x={3}>
+        <Camping scale={0.6} />
+      </group>
+      <Environment preset="sunset" />
     </>
   );
 };
--- src/components/camping/index.jsx
+++ src/components/camping/index.jsx
@@ -1,6 +1,4 @@
-import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import { Experience } from './Experience';
 
 const App = () => {
@@ -8,13 +6,8 @@ const App = () => {
     <>
       <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
         <color attach="background" args={['#171720']} />
+        <Experience />
         <fog attach="fog" args={['#171720', 10, 30]} />
-        <Suspense>
-          <Experience />
-        </Suspense>
-        <EffectComposer>
-          <Bloom mipmapBlur intensity={1.2} />
-        </EffectComposer>
       </Canvas>
     </>
   );
